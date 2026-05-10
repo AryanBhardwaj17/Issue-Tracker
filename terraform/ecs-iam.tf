0b07c9c9254979e2150ef5442b9c373895e81d92
KVasanth <k_vasanth@epam.com>
2026-05-10 19:03:43 +0530

    ci: allow deployment branch for CI/CD pipeline testing

--- terraform/ecs-iam.tf
+++ terraform/ecs-iam.tf
@@ -150,8 +150,8 @@ resource "aws_iam_role" "gitlab_ci" {
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
-          StringEquals = {
-            "gitlab.com:sub" = "project_path:${var.gitlab_project_path}:ref_type:branch:ref:main"
+          StringLike = {
+            "gitlab.com:sub" = "project_path:${var.gitlab_project_path}:ref_type:branch:ref:*"
           }
         }
       }
